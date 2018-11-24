95eee8e8a174a8e55a34b54433dad40073d647e4
Jun <hanjun.1998@gmail.com>
2018-11-24 08:22:34 +0000

    added testing to func that calcs area of rects

--- structs/geometry_test.go
+++ structs/geometry_test.go
@@ -13,4 +13,13 @@ func TestPerimeter(t *testing.T) {
 			t.Errorf("got %f, wanted %f", got, want)
 		}
 	})
+
+	t.Run("calculating the area of a recatangle givben a height and a width", func(t *testing.T) {
+		got := Area(2.5, 2.0)
+		want := 5.0
+
+		if got != want {
+			t.Errorf("got %f, wanted %f, got, want")
+		}
+	})
 }
