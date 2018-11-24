2b1aecd52d0b6e7acfda1ba15cd96c6e017fcb19
Jun <hanjun.1998@gmail.com>
2018-11-24 11:01:19 +0000

    refactored code, added rectangle type

--- structs/geometry_test.go
+++ structs/geometry_test.go
@@ -5,21 +5,26 @@ import (
 )
 
 func TestPerimeter(t *testing.T) {
-	t.Run("calculating the perimeter of a rectangle given a height and a width", func(t *testing.T) {
-		got := Perimeter(2.5, 2.0)
-		want := 9.0
+	rect := Rectangle{2.5, 2.0}
 
+	assertCorrectMessage := func(t *testing.T, got, want float64) {
+		//t.Helper()
 		if got != want {
 			t.Errorf("got %f, wanted %f", got, want)
 		}
+	}
+
+	t.Run("calculating the perimeter of a rectangle given a height and a width", func(t *testing.T) {
+		got := Perimeter(rect)
+		want := 9.0
+
+		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("calculating the area of a recatangle givben a height and a width", func(t *testing.T) {
-		got := Area(2.5, 2.0)
+		got := Area(rect)
 		want := 5.0
 
-		if got != want {
-			t.Errorf("got %f, wanted %f, got, want")
-		}
+		assertCorrectMessage(t, got, want)
 	})
 }
