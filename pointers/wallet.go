2fa7068de61a944a18d1aca79bfa60cfe2a3e4df
Jun <hanjun.1998@gmail.com>
2018-11-26 11:08:21 +0000

    refactored code, added a withdraw method to the wallet type

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -6,14 +6,24 @@ import (
 
 type Bitcoin int
 
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
 	balance Bitcoin
 }
 
 func (w *Wallet) Deposit(amount Bitcoin) {
-	fmt.Println("adr of balance in test: ", &w.balance)
+	fmt.Println("balance of wallet: ", w.balance)
 
 	w.balance += amount
+}
+
+func (w *Wallet) Withdraw(amount Bitcoin) {
+	fmt.Println("balance of wallet: ", w.balance)
+
+	w.balance -= amount
 }
 
 func (w *Wallet) Balance() Bitcoin {
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -6,16 +6,33 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
 
-	got := wallet.Balance()
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		got := wallet.Balance()
 
-	fmt.Println("adr of balance in test: ", &wallet.balance)
+		fmt.Println("balance of wallet: ", &wallet.balance)
 
-	want := Bitcoin(10)
-
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(10)
+
+		want := Bitcoin(10)
+
+		assertBalance(t, wallet, want)
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{10}
+		wallet.Withdraw(10)
+
+		want := Bitcoin(0)
+
+		assertBalance(t, wallet, want)
+	})
+
 }
