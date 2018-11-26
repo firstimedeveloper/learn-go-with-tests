bc9d6c63da0427d62d427f81b97410137e6868f4
Jun <hanjun.1998@gmail.com>
2018-11-26 09:53:22 +0000

    refactored code, implemented table driven tests, added a triangle type

--- structs/geometry.go
+++ structs/geometry.go
@@ -14,6 +14,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Width  float64
+	Height float64
+}
+
 func (r Rectangle) Perimeter() float64 {
 	return 2 * (r.Width + r.Height)
 }
@@ -28,4 +33,12 @@ func (c Circle) Perimeter() float64 {
 
 func (c Circle) Area() float64 {
 	return 3.14 * c.Radius * c.Radius
+}
+
+func (t Triangle) Perimeter() float64 {
+	return 0
+}
+
+func (t Triangle) Area() float64 {
+	return t.Width * t.Height / 2
 }
--- structs/geometry_test.go
+++ structs/geometry_test.go
@@ -4,46 +4,86 @@ import (
 	"testing"
 )
 
-func TestGeometry(t *testing.T) {
+//	func TestGeometry(t *testing.T) {
+
+// 	checkArea := func(t *testing.T, s Shape, want float64) {
+// 		//t.Helper()
+// 		got := s.Area()
+// 		if got != want {
+// 			t.Errorf("got %f, wanted %f", got, want)
+// 		}
+// 	}
+// 	checkPerimeter := func(t *testing.T, s Shape, want float64) {
+// 		//t.Helper()
+// 		got := s.Perimeter()
+// 		if got != want {
+// 			t.Errorf("got %f, wanted %f", got, want)
+// 		}
+// 	}
+
+// 	t.Run("calculating the perimeter of a rectangle given a height and a width", func(t *testing.T) {
+// 		want := 9.0
+
+// 		checkPerimeter(t, r, want)
+// 	})
+
+// 	t.Run("calculating the area of a recatangle givben a height and a width", func(t *testing.T) {
+// 		want := 5.0
+
+// 		checkArea(t, r, want)
+// 	})
+
+// 	t.Run("calculating the perimeter of a circle", func(t *testing.T) {
+// 		want := 6.28
+
+// 		checkPerimeter(t, c, want)
+// 	})
+
+// 	t.Run("calculating the area of a circle", func(t *testing.T) {
+// 		want := 3.14
+
+// 		checkArea(t, c, want)
+// 	})
+// }
+
+func TestArea(t *testing.T) {
+	r := Rectangle{2.5, 2.0}
+	c := Circle{1.0}
+	tr := Triangle{2.5, 2.0}
+
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{r, 5.0},
+		{c, 3.14},
+		{tr, 2.5},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %f want %f", got, tt.want)
+		}
+	}
+}
+
+func TestPerimeter(t *testing.T) {
 	r := Rectangle{2.5, 2.0}
 	c := Circle{1.0}
 
-	checkArea := func(t *testing.T, s Shape, want float64) {
-		//t.Helper()
-		got := s.Area()
-		if got != want {
-			t.Errorf("got %f, wanted %f", got, want)
-		}
-	}
-	checkPerimeter := func(t *testing.T, s Shape, want float64) {
-		//t.Helper()
-		got := s.Perimeter()
-		if got != want {
-			t.Errorf("got %f, wanted %f", got, want)
-		}
+	perimeterTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{r, 9.0},
+		{c, 6.28},
 	}
 
-	t.Run("calculating the perimeter of a rectangle given a height and a width", func(t *testing.T) {
-		want := 9.0
-
-		checkPerimeter(t, r, want)
-	})
-
-	t.Run("calculating the area of a recatangle givben a height and a width", func(t *testing.T) {
-		want := 5.0
-
-		checkArea(t, r, want)
-	})
-
-	t.Run("calculating the perimeter of a circle", func(t *testing.T) {
-		want := 6.28
-
-		checkPerimeter(t, c, want)
-	})
-
-	t.Run("calculating the area of a circle", func(t *testing.T) {
-		want := 3.14
-
-		checkArea(t, c, want)
-	})
+	for _, tt := range perimeterTests {
+		got := tt.shape.Perimeter()
+		if got != tt.want {
+			t.Errorf("got %f want %f", got, tt.want)
+		}
+	}
 }
