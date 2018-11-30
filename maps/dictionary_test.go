270c9948738aa900ebb069de1bcf8d2cf1c717c3
Jun <hanjun.1998@gmail.com>
2018-11-30 11:15:06 +0000

    added an add method to the dictionary type

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -4,18 +4,46 @@ import (
 	"testing"
 )
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if want != got {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
+
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	// t.Run("known word", func(t *testing.T) {
+	// 	got, _ := dictionary.Search("test")
+	// 	want := "this is just a test"
 
-	assertStrings(t, got, want)
+	// 	assertError(t, got, want)
+	// })
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		if got == nil {
+			t.Fatal("expected an error")
+		}
+
+		assertError(t, got, ErrNotFound)
+	})
+
 }
 
-func assertStrings(t *testing.T, got, want string) {
+func assertError(t *testing.T, got, want error) {
 	//t.Helper()
 	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
+		t.Errorf("got error '%s' want error '%s'", got, want)
 	}
 }
