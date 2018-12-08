5761275051ad422f1053844c83c27ad311c91d19
Jun <hanjun.1998@gmail.com>
2018-12-08 01:07:05 +0000

    refactored code so that sleep uses mocking

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -4,15 +4,38 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
-func main() {
-	Countdown(os.Stdout)
+type Sleeper interface {
+	Sleep()
 }
 
-func Countdown(out io.Writer) {
+type SpySleeper struct {
+	Calls int
+}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func main() {
+	sleeper := &DefaultSleeper{}
+
+	Countdown(os.Stdout, sleeper)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := 3; i > 0; i-- {
+		sleeper.Sleep()
 		fmt.Fprintf(out, "%d\n", i)
 	}
+	sleeper.Sleep()
 	fmt.Fprint(out, "Go!")
 }
