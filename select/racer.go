c034b7336ac4b70a64402542394233706ecb269b
Jun <hanjun.1998@gmail.com>
2018-12-14 10:34:36 +0000

    enough code in the Racer function to run the test

--- select/racer.go
+++ select/racer.go
@@ -1,6 +1,22 @@
 package main
 
-func Racer(urls ...string) string {
+import (
+	"net/http"
+	"time"
+)
 
-	return urls[1]
+func Racer(a, b string) (winner string) {
+	startA := time.Now()
+	http.Get(a)
+	aDuration := time.Since(startA)
+
+	startB := time.Now()
+	http.Get(b)
+	bDuration := time.Since(startB)
+
+	if aDuration < bDuration {
+		return a
+	}
+	return b
+
 }
