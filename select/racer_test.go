eff7f10598af31785661c3d955788c3ed2b65415
Jun <hanjun.1998@gmail.com>
2018-12-14 10:51:19 +0000

    using httptest to create a mock server

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestRacer(t *testing.T) {
-	slowURL := "http://www.facebook.com"
-	fastURL := "http://www.quii.co.uk"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	slowURL := slowServer.URL
+	fastURL := slowServer.URL
 
 	want := fastURL
 	got := Racer(slowURL, fastURL)
@@ -14,4 +26,7 @@ func TestRacer(t *testing.T) {
 	if got != want {
 		t.Errorf("got '%s', want '%s'", got, want)
 	}
+
+	slowServer.Close()
+	fastServer.Close()
 }
