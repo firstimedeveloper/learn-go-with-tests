c43e4cdbe4b7c9bd7755da687640689cbf44a29e
Jun <hanjun.1998@gmail.com>
2018-12-15 07:59:51 +0000

    refactored to reduce repeated code

--- app/server_test.go
+++ app/server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,7 +9,7 @@ import (
 
 func TestGETPlayers(t *testing.T) {
 	t.Run("returns Pepper's score", func(t *testing.T) {
-		req, _ := http.NewRequest(http.MethodGet, "/players/Pepper", nil)
+		req := newGetScoreRequest("Pepper")
 		resp := httptest.NewRecorder()
 
 		PlayerServer(resp, req)
@@ -16,21 +17,28 @@ func TestGETPlayers(t *testing.T) {
 		got := resp.Body.String()
 		want := "20"
 
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
+		assertResponseBody(t, got, want)
 	})
 	t.Run("returns Floyd's score", func(t *testing.T) {
-		req, _ := http.NewRequest(http.MethodGet, "/players/Floyd", nil)
+		req := newGetScoreRequest("Floyd")
 		resp := httptest.NewRecorder()
 
 		PlayerServer(resp, req)
 
 		got := resp.Body.String()
 		want := "10"
+		assertResponseBody(t, got, want)
 
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
 	})
+}
+
+func newGetScoreRequest(player string) *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", player), nil)
+	return req
+}
+
+func assertResponseBody(t *testing.T, got, want string) {
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
 }
