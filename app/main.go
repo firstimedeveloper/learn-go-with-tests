b13106c6c1446f7ee0c9339dcc5f9387e89716f7
Jun <hanjun.1998@gmail.com>
2018-12-15 07:28:28 +0000

    added a main file: that listens on port 8080

--- app/main.go
+++ app/main.go
@@ -1,1 +1,13 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	handler := http.HandlerFunc(PlayerServer)
+	if err := http.ListenAndServe(":8080", handler); err != nil {
+		log.Fatalf("could not listen on port 8080 %v", err)
+	}
+}
