0d8f0a4caeb5f40311f3d90332d77d33fb89920d
Jun <hanjun.1998@gmail.com>
2018-12-29 11:27:32 +0000

    new test

--- app/server_test.go
+++ app/server_test.go
@@ -61,6 +61,21 @@ func TestGETPlayers(t *testing.T) {
 		assertStatus(t, resp.Code, http.StatusNotFound)
 	})
 }
+func TestStoreWins(t *testing.T) {
+	store := StubPlayerStore{
+		map[string]int{},
+	}
+	server := &PlayerServer{&store}
+
+	t.Run("it returns accepted on POST", func(t *testing.T) {
+		request, _ := http.NewRequest(http.MethodPost, "/players/Pepper", nil)
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusAccepted)
+	})
+}
 
 func newGetScoreRequest(player string) *http.Request {
 	req, _ := http.NewRequest(http.MethodGet, fmt.Sprintf("/players/%s", player), nil)
